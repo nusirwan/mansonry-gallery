0721318c17b78c3669b885125d12cdf61e738c3d
nusirwan <inbox@iwan.im>
2019-06-13 11:30:38 +0700

    App: import, use & accept  Gallery & data

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 
+import IMAGES from './_DATA.json';
+
 import GlobalStyle from './base-styles/globalStyle';
 import Container from './components/Container';
+import Gallery from './components/Gallery';
 
 const App = () => {
 	return (
@@ -15,7 +18,7 @@ const App = () => {
 						exact
 						path="/"
 						render={ props =>
-							<h1 { ...props }>Mansonry Gallery</h1>
+							<Gallery { ...props } images={ IMAGES } />
 						}
 					/>
 				</Switch>
